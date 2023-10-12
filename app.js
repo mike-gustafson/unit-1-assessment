1e6767f1c38d725363600efefb5eabc2ff7fd744
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-10-11 21:11:19 -0700

    feat: adder slider to convert to alpha

--- app.js
+++ app.js
@@ -2,33 +2,46 @@ const output = document.getElementById('output');
 const addButton = document.getElementById('add-button');
 const subtractButton = document.getElementById('subtract-button');
 const input = document.querySelector('input');
+const slider = document.querySelector('.input-slider');
+const sliderContainer = document.getElementById('slider-container');
+let currentNumber = 1;
+
+function updateOutputValue() {
+    if (slider.checked) {
+        let resultChar = String.fromCharCode((Math.abs(currentNumber) % 26) + 96);
+        output.textContent = resultChar;
+    } else {
+        output.textContent = currentNumber;
+    }
+    updateOutputColor();
+}
 
 addButton.addEventListener('click', function (e) {
     e.preventDefault();
-    const inputValue = input.value;
-    const currentValue = output.textContent;
+    const inputValue = parseFloat(input.value);
     if (!isNaN(inputValue)) {
-        const result = parseFloat(currentValue) + parseFloat(inputValue);
-        output.textContent = result;
-        updateOutputColor(result);
+        currentNumber += inputValue;
+        updateOutputValue();
     }
 });
 
-subtractButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    const inputValue = input.value;
-    const currentValue = output.textContent;
+subtractButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputValue = parseFloat(input.value);
     if (!isNaN(inputValue)) {
-        const result = parseFloat(currentValue) - parseFloat(inputValue);
-        output.textContent = result;
-        updateOutputColor(result);
+        currentNumber -= inputValue;
+        updateOutputValue();
     }
+});
+
+sliderContainer.addEventListener('change', function () {
+    updateOutputValue();
 });
 
 function updateOutputColor(result) {
-    if (result < 0) {
+    if (currentNumber < 0) {
         output.style.color = 'red';
     } else {
         output.style.color = '';
-    }
+    }    
 }
