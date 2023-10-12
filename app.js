22bbca1a7bfcfc1de6ba1538a12adef610d3ebb4
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-10-11 19:58:20 -0700

    feat: added basic alpha functionality

--- app.js
+++ app.js
@@ -2,26 +2,40 @@ const output = document.getElementById('output');
 const addButton = document.getElementById('add-button');
 const subtractButton = document.getElementById('subtract-button');
 const input = document.querySelector('input');
-
+let currentChar = 'a';
 addButton.addEventListener('click', function (e) {
     e.preventDefault();
-    const inputValue = parseFloat(input.value);
-    const currentValue = parseFloat(output.textContent);
+    const inputValue = input.value;
+    const currentValue = output.textContent;
     if (!isNaN(inputValue)) {
-        const result = currentValue + inputValue;
+        const result = parseFloat(currentValue) + parseFloat(inputValue);
         output.textContent = result;
         updateOutputColor(result);
+    } else {
+        if (currentChar === 'z') {
+            currentChar = 'a';
+        } else {
+            currentChar = String.fromCharCode(currentChar.charCodeAt(0) + 1);
+        }
+        output.textContent = currentChar;
     }
 });
 
 subtractButton.addEventListener('click', function (e) {
     e.preventDefault();
-    const inputValue = parseFloat(input.value);
-    const currentValue = parseFloat(output.textContent);
+    const inputValue = input.value;
+    const currentValue = output.textContent;
     if (!isNaN(inputValue)) {
-        const result = currentValue - inputValue;
+        const result = parseFloat(currentValue) - parseFloat(inputValue);
         output.textContent = result;
         updateOutputColor(result);
+    } else {
+        if (currentChar === 'a') {
+            currentChar = 'z';
+        } else {
+            currentChar = String.fromCharCode(currentChar.charCodeAt(0) - 1);
+        }
+        output.textContent = currentChar;
     }
 });
 
